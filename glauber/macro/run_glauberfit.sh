6d8b4446450a6aff777dc453a9db5a0e05b6830d
OleksiiLubynets <lubynets@lxbk0200.gsi.de>
2019-03-06 12:32:31 +0100

    modify run_glauber

--- glauber/macro/run_glauberfit.sh
+++ glauber/macro/run_glauberfit.sh
@@ -2,18 +2,18 @@
 #SBATCH -J GlauberFit
 #SBATCH -o out/%j.out.log
 #SBATCH -e error/%j.err.log
-#SBATCH --time=08:00:00
-#SBATCH --array=0-50
+#SBATCH --time=00:20:00
+#SBATCH --array=0-5
 
-F=$(echo "$SLURM_ARRAY_TASK_ID * 0.02" | bc -l)
-FILE=/lustre/nyx/cbm/users/klochkov/na61/pbpb/30agev/data/flow/psd_centr/pbpb_30agev_data_config/all_corrections/pi_neg_debug/qa.root
+INDEX=$(echo "$SLURM_ARRAY_TASK_ID * 0.2" | bc -l)
+FILE=/lustre/nyx/cbm/users/$USER/soft/centrality/input/test_input.root
 
 K0=1
-K1=30
+K1=10
 MULTMIN=$1
 
-EXE_DIR=/lustre/nyx/cbm/users/klochkov/soft/CentralityFramework/build/
-OUT_DIR=/lustre/nyx/cbm/users/klochkov/data/centrality/na61/pb30pb/glauber/npart/pbpb_30agev_data_pions/$MULTMIN/
+EXE_DIR=/lustre/nyx/cbm/users/$USER/soft/centrality/build/
+OUT_DIR=/lustre/nyx/cbm/users/$USER/test/centrality/$MULTMIN/
 
 source /lustre/nyx/cbm/users/klochkov/soft/root6/root-6.10.08/install/bin/thisroot.sh
 
