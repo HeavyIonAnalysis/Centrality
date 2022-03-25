985bf5d3fc8d1a7185bb2d63b5a30c047217a6e1
viktorklochkov <viktorklochkov@users.noreply.github.com>
2022-03-25 09:06:32 +0000

    deploy: 6aa9b5a86f71d4343e108932e1dd59da3a7b85ac

--- search/all_6.js
+++ search/all_6.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['setnbdhist_0',['SetNBDhist',['../classGlauber_1_1Fitter.html#adedf9e8696e8e5cf7734015798e2b658',1,'Glauber::Fitter']]]
+  ['setborders_0',['SetBorders',['../classCentrality_1_1BordersFinder.html#a1830067f1e0df52ce80fc7668a9e8031',1,'Centrality::BordersFinder']]],
+  ['setnbdhist_1',['SetNBDhist',['../classGlauber_1_1Fitter.html#adedf9e8696e8e5cf7734015798e2b658',1,'Glauber::Fitter']]]
 ];
